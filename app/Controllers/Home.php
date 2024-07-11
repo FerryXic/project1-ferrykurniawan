75a377fb8be011ef5c9e2686e61ac8cb805bd9dc
ferry <ferrykurniawan.exc@gmail.com>
2024-07-11 14:48:29 +0700

    controller ke halaman login

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'title' => 'Seri Tutorial CodeIgniter 4: Login dan Register @ qadrlabs.com'
+        ];
+
+        return view('home', $data);
     }
 }
